22b0f63e09936a5bb9bdfd6553facc6c369ea1f3
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-12-23 10:49:06 +0100

    sort output and add slots

--- src/models.rs
+++ src/models.rs
@@ -45,13 +45,13 @@ impl Chain {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct ValidatorNomination {
     pub nominator: String,
     pub stake: Balance,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Validator {
     pub stash: String,
     pub self_stake: Balance,
@@ -73,6 +73,7 @@ pub struct ValidatorOutput {
     pub stash: String,
     pub self_stake: String,
     pub total_stake: String,
+    pub slot: u32,
     pub commission: f64,
     pub blocked: bool,
     pub nominations_count: usize,
@@ -181,6 +182,9 @@ pub struct SimulationResultOutput {
 
 impl SimulationResult {
     pub fn to_output(&self, chain: Chain) -> SimulationResultOutput {
+        let mut vals = self.active_validators.clone();
+        vals.sort_by(|a, b| b.total_stake.cmp(&a.total_stake));
+
         SimulationResultOutput {
             run_parameters: self.run_parameters.clone(),
             staking_stats: StakingStatsOutput {
@@ -188,11 +192,12 @@ impl SimulationResult {
                 lowest_staked: chain.format_stake(self.staking_stats.lowest_staked),
                 avg_staked: chain.format_stake(self.staking_stats.avg_staked),
             },
-            active_validators: self.active_validators.iter().map(|v| {
+            active_validators: vals.iter().enumerate().map(|(i, v)| {
                 ValidatorOutput {
                     stash: v.stash.clone(),
                     self_stake: chain.format_stake(v.self_stake),
                     total_stake: chain.format_stake(v.total_stake),
+                    slot: i as u32,
                     commission: v.commission,
                     blocked: v.blocked,
                     nominations_count: v.nominations_count,
